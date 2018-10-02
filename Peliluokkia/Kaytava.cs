fb3e80f82ec96538f11cb7cd99cf56816f2ad555
forceberry <otto.forsberg@gmail.com>
2018-10-02 11:51:00 +0300

    Lisätty pääsy porraskäytävään

--- Peliluokkia/Kaytava.cs
+++ Peliluokkia/Kaytava.cs
@@ -8,11 +8,47 @@ namespace Peliluokkia
 {
     public class Kaytava
     {
+        string vastaus;
         public void Avaa()
         {
             Console.WriteLine("Olet hämärässä käytävässä, jonka toisessa päässä on keittiö (A), toisessa porraskäytävä (B).\n\n" +
-                "Lisäksi käytävän varrelta löytyvät luokat Lovelace (C), Hopper (D), Jobs (E) ja Gosling (F) sekä konsolipelinurkkaus (G) ja varasto (H).");
-            Console.ReadLine();
+                "Lisäksi käytävän varrelta löytyy C#-ryhmän Hejlsberg-luokka (C), pienet huoneet Lovelace (D), Hopper (E), Jobs (F) ja Gosling (G) sekä konsolipelinurkkaus (H) ja varasto (I).");
+            vastaus = Console.ReadLine();
+            vastaus = vastaus.ToUpper();
+
+            switch (vastaus)
+            {
+                case "A":
+                    Console.WriteLine("Lähdet kävelemään kohti keittiötä.\n");
+                    Avaa();
+                    break;
+                case "B":
+                    Console.WriteLine("Avaat käytävän perältä löytyvän hätäuloskäynnin oven porraskäytävään.");
+                    Porraskaytava porraskaytava = new Porraskaytava();
+                    porraskaytava.Avaa();
+                    break;
+                case "C":
+                    Console.WriteLine("Avaat ovat C#-ryhmän luokkaan.");
+                    Hejlsberg hejlsberg = new Hejlsberg();
+                    hejlsberg.Avaa();
+                    break;
+                case "D":
+                case "E":
+                case "F":
+                case "G":
+                case "I":
+                    Console.WriteLine("Väännät kahvasta, mutta toteat oven olevan lukossa.");
+                    Avaa();
+                    break;
+                case "H":
+                    Console.WriteLine("Koska sähkö ovat poikki, pysyy pelikonsoli mykkänä");
+                    Avaa();
+                    break;
+                default:
+                    Console.WriteLine("Epäkelpo valinta.");
+                    Avaa();
+                    break;
+            }
         }
     }
 }
--- Peliluokkia/Porraskaytava.cs
+++ Peliluokkia/Porraskaytava.cs
@@ -8,10 +8,29 @@ namespace Peliluokkia
 {
     public class Porraskaytava
     {
+        string vastaus;
         public void Avaa()
         {
             Console.WriteLine("Hätäuloskäyntiä ilmaisevan lampun hämyisän vihreä valo valaisee alaspäin johtavia portaita (A). Vieressäsi on ovi takaisin Academyn käytävään (B).");
-            Console.ReadLine();
+            vastaus = Console.ReadLine();
+            vastaus = vastaus.ToUpper();
+
+            switch (vastaus)
+            {
+                case "A":
+                    Console.WriteLine("Lähdet kävelemään portaita alas kelmeän vihertävässä valossa.\n");
+                    Avaa();
+                    break;
+                case "B":
+                    Console.WriteLine("Avaat oven käytävään.");
+                    Kaytava kaytava = new Kaytava();
+                    kaytava.Avaa();
+                    break;
+                default:
+                    Console.WriteLine("Epäkelpo valinta.");
+                    Avaa();
+                    break;
+            }
         }
     }
 }
