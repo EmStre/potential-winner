2e9e7553bfd8db510a4b7aac2458cfbdb1f30c46
forceberry <otto.forsberg@gmail.com>
2018-10-02 15:01:05 +0300

    Tekstin muotoilua.

--- Konsoli/Program.cs
+++ Konsoli/Program.cs
@@ -13,9 +13,9 @@ namespace Konsoli
         {
             Hejlsberg hejlsberg = new Hejlsberg();
             Kaytava kaytava = new Kaytava();
-            Console.WriteLine("Istut perjantai-iltana yksn C#-ryhmän Hejlsberg-luokassa kun huomaat kattolamppujen valon värisevän.\n" +
-                "Hetken päästä säpsähdät, kun suuren pamauksen saattelemana valot yhtäkkiä sammuvat ja kannettava tietokoneesi pimenee.\n" +
-                "Vilkaiset kännykästä kelloa ja toteat sen olevan 23:18. Huomaat myös kännykän kuuluvuuskentän kadonneen.");
+            Console.WriteLine("Istut perjantai-iltana yksin C#-ryhmän Hejlsberg-luokassa kun huomaat kattolamppujen valon värisevän.\n" +
+                "Hetken päästä säpsähdät, kun suuren pamauksen saattelemana valot yhtäkkiä sammuvat ja kannettava tietokoneesi samalla pimenee.\n" +
+                "Istut hetken yllättyneenä pimeässä luokassa ihmetellen tapahtunuttat. Vilkaiset kännykästäsi kelloa ja toteat sen olevan 23:18. Huomaat samalla kännykän kuuluvuuskentän olevan nollassa.");
             hejlsberg.Avaa();
         }
     }
