87a833c3da250ba20dd9d8ae86627c835c3d0012
emmaTu <emma.n.tuovinen@gmail.com>
2018-10-02 15:23:18 +0300

    päivitetty caset keittokomeroon

--- Peliluokkia/Keittokomero.cs
+++ Peliluokkia/Keittokomero.cs
@@ -13,23 +13,27 @@ namespace Peliluokkia
             string vastaus;
             Console.WriteLine("Hapuilet pimeässä tiskipöydän luo.\n" +
                 "Pöydältä erottuu esine, jonka tunnistat ensiapulaukuksi.\n" +
-                "Kädelläsi tavoitat myös vesihanan. (A) vie sinut ruokailutilaan tai voit siirtyä takaisin (B) keittiön puolelle.\n" +
-                "WC-tilan (C) ovi näyttäisi olevan hieman raollaan. \n");
+                "Kädelläsi tavoitat myös vesihanan (A). (B) vie sinut ruokailutilaan tai voit siirtyä takaisin (C) keittiön puolelle.\n" +
+                "WC-tilan (D) ovi näyttäisi olevan hieman raollaan. \n");
             vastaus = Console.ReadLine();
             vastaus = vastaus.ToUpper();
 
             switch (vastaus)
             {
                 case "A":
-                    Console.WriteLine("Siirryt ruokailutilaan.\n");
+                    Console.WriteLine("Helkkarin kiva, juoksevaa vettä ei tule.\n");
                     Avaa();
                     break;
                 case "B":
+                    Console.WriteLine("Siirryt ruokailutilaan.\n");
+                    Avaa();
+                    break;
+                case "C":
                     Console.WriteLine("Kävelet keittiöön.\n");
                     Keittio keittio = new Keittio();
                     keittio.Avaa();
                     break;
-                case "C":
+                case "D":
                     Console.WriteLine("Astut WC-tilaan.\n");
                     WC vessa = new WC();
                     vessa.Avaa();
