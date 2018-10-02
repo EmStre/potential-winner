c7d24628509a8f512a73ed6c07b8606da3227971
forceberry <otto.forsberg@gmail.com>
2018-10-02 10:26:59 +0300

    Sisältöä Hejlsbergiin

---
+++ Huoneet/Hejlsberg.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Huoneet
+{
+    public class Hejlsberg
+    {
+        public void Avaa()
+        {
+            Console.WriteLine("C#-ryhmän luokka on tyhjä ja hämärä, lähes täysin pimeä. Joten kuten erotat valonkatkaisijan (A), kaksi ovea käytävään (B) ja oven Java-ryhmän Hawking-luokkaan (C).");
+            Console.Readline();
+        }
+    }
+}
