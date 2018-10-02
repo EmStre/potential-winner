4932228f850bea498190dd7a1acec4d3c0279e70
forceberry <otto.forsberg@gmail.com>
2018-10-02 11:11:30 +0300

    Lisätty valinta Hejlsbergiin

--- Huoneet/Hejlsberg.cs
+++ Huoneet/Hejlsberg.cs
@@ -8,10 +8,30 @@ namespace Huoneet
 {
     public class Hejlsberg
     {
+        string vastaus;
         public void Avaa()
         {
             Console.WriteLine("C#-ryhmän luokka on tyhjä ja hämärä, lähes täysin pimeä. Joten kuten erotat valonkatkaisijan (A), kaksi ovea käytävään (B) ja oven Java-ryhmän Hawking-luokkaan (C).");
-            Console.Readline();
+            vastaus = Console.ReadLine();
+            vastaus = vastaus.ToUpper();
+
+            switch (vastaus)
+            {
+                case "A":
+                    Console.WriteLine("Painat valonkatkaisijaa, mutta mitään ei tapahdu. Sähköt tosiaan taitavat olla poikki.");
+                    break;
+                case "B":
+                    Console.WriteLine("Avaat oven käytävään.");
+                    Huoneet.Kaytava.Avaa();
+                    break;
+                case "C":
+                    Console.WriteLine("Väännät Java-ryhmän huoneen oven kahvasta, mutta toteat oven olevan lukossa.");
+                    break;
+                default:
+                    Console.WriteLine("Epäkelpo valinta.");
+                    Avaa();
+                    break;
+            }
         }
     }
 }
