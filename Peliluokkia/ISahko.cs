e4471f1a3ee2afba435f1e766980720196c5a148
emmaTu <emma.n.tuovinen@gmail.com>
2018-10-03 12:15:06 +0300

    Lisätty sähkö-rajapinta ja muokattu varastoa

---
+++ Peliluokkia/ISahko.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Peliluokkia
+{
+    interface ISahko
+    {
+        void SahkotPaalla();
+
+        void SahkotPois();
+
+        bool Electricity { get; }
+    }
+}
--- Peliluokkia/Varasto.cs
+++ Peliluokkia/Varasto.cs
@@ -6,34 +6,138 @@ using System.Threading.Tasks;
 
 namespace Peliluokkia
 {
-    class Varasto
+    class Varasto : ILamppu, ISahko
     {
         string vastaus;
+
+        private bool lamppuPäällä = false;
+
+        private bool sahkoPaalla = false;
+
+        public bool LamppuPäällä
+        {
+            get
+            {
+                return lamppuPäällä;
+            }
+        }
+
+        public bool Electricity
+        {
+            get
+            {
+                return sahkoPaalla;
+            }
+        }
+
+        Varasto lamppu;
+        Varasto sahkot;
+
+
         public void Avaa()
         {
             Console.WriteLine("Varasto on pilkkopimeä ja täynnä viinalaatikoita. Pimeässä on hankala lähteä etenemään.\n" +
-                "Muistat, että sähkökaappi löytyy varaston perältä! Olisikohan siellä sellainen varavirtakytkin kuten leffoissa.");
+                "Muistat, että sähkökaappi löytyy varaston perältä! Olisikohan siellä sellainen varavirtakytkin kuten leffoissa." +
+                "Voit myös poistua takaisin käytävälle (A).");
             vastaus = Console.ReadLine();
             vastaus = vastaus.ToUpper();
 
-            switch (vastaus)
+            if (Inventaario.esineet.Contains("taskulamppu"))
             {
-                case "A":    
-                    break;
-                case "KASSI":
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    Inventaario inventaario = new Inventaario();
-                    Console.WriteLine(inventaario);
-                    Console.ResetColor();
-                    Avaa();
-                    break;
-                default:
-                    Console.ForegroundColor = ConsoleColor.Cyan;
-                    Console.WriteLine("Epäkelpo valinta.\n");
-                    Console.ResetColor();
-                    Avaa();
-                    break;
+
+                switch (vastaus)
+                {
+                    case "TASKULAMPPU PÄÄLLE":
+                        lamppu.Päällä();
+                        ValoisaVarasto();
+                        break;
+                    case "LAMPPU PÄÄLLE":
+                        lamppu.Päällä();
+                        ValoisaVarasto();
+                        break;
+                    case "AVAA LAMPPU":
+                        lamppu.Päällä();
+                        ValoisaVarasto();
+                        break;
+                    case "LAITA LAMPPU PÄÄLLE":
+                        lamppu.Päällä();
+                        ValoisaVarasto();
+                        break;
+                    case "KYTKE LAMPPU PÄÄLLE":
+                        lamppu.Päällä();
+                        ValoisaVarasto();
+                        break;
+                    case "A":
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.WriteLine("Astut takaisin käytävään.\n");
+                        Console.ResetColor();
+                        Kaytava kaytava = new Kaytava();
+                        kaytava.Avaa();
+                        break;
+                    case "KASSI":
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Inventaario inventaario = new Inventaario();
+                        Console.WriteLine(inventaario);
+                        Console.ResetColor();
+                        Avaa();
+                        break;
+                    default:
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.WriteLine("Epäkelpo valinta.\n");
+                        Console.ResetColor();
+                        Avaa();
+                        break;
+                }
             }
+        }
+
+        public void ValoisaVarasto()
+        {
+            Console.WriteLine("Noniin! Nyt pääset etenemään laatikoiden yli varaston perälle ja siellähän se sähkökaappi pilkottaa.");
+            vastaus = Console.ReadLine();
+            vastaus = vastaus.ToUpper();
+
+            switch(vastaus)
+            {
+                case "SÄHKÖT PÄÄLLE":
+                    sahkot.SahkotPaalla();
+
+                    break;
+                     
+            }
+        }
+
+        public void PoisPäältä()
+        {
+            lamppuPäällä = false;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Taskulamppu sammui");
+            Console.ResetColor();
+        }
+
+        public void Päällä()
+        {
+            lamppuPäällä = true;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Taskulamppu on päällä");
+            Console.ResetColor();
+        }
+
+        public void SahkotPaalla()
+        {
+            sahkoPaalla = true;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Sähköt päällä!");
+            Console.ResetColor();
+
+        }
+
+        public void SahkotPois()
+        {
+            sahkoPaalla = false;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Sähköt pois päältä.");
+            Console.ResetColor();
         }
     }
 }
